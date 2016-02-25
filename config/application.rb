24217106196da640f7daac393c9ed2e1a8813e71
Daniel Filho <afilhodaniel@gmail.com>
2016-02-24 22:55:59 -0300

    Fix bug on dotenv production

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 
 module CinepolisApi
   class Application < Rails::Application
-    Dotenv.load
+    Dotenv.load if Rails.env.development?
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
