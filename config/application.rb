278b6ced70ee04015b9730efaf3123e611aae621
Daniel Filho <afilhodaniel@gmail.com>
2016-02-24 22:56:58 -0300

    Fix bug on dotenv production

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'dotenv'
+require 'dotenv' if Rails.env.development?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module CinepolisApi
   class Application < Rails::Application
     Dotenv.load if Rails.env.development?
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
