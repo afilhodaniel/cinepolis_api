eb3f821e6025ab1942813067bf06b9da86099bfe
Daniel Filho <afilhodaniel@gmail.com>
2016-05-24 12:16:07 -0300

    Add query by movie_theater_id on movies endpoints

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -20,6 +20,31 @@ module Api
           end
         else
           @movie = movie
+
+          cities = []
+          to_delete = []
+
+          if query_params[:movie_theater_id]
+            movie[:cities].each do |city|
+              city[:city][:movie_theaters].each do |movie_theater|
+                if movie_theater[:id] == query_params[:movie_theater_id]
+                  cities << city
+                else
+                  to_delete << movie_theater
+                end
+              end
+            end
+
+            cities.each do |city|
+              city[:city][:movie_theaters].each do |movie_theater|
+                if to_delete.include?(movie_theater)
+                  city[:city][:movie_theaters].delete(movie_theater)
+                end
+              end
+            end
+
+            @movie[:cities] = cities
+          end
         end
       end
 
@@ -27,6 +52,10 @@ module Api
       
         def movie_params
           params.require(:movie)
+        end
+
+        def query_params
+          params.permit(:movie_theater_id)
         end
 
     end
